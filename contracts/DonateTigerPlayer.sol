8118fcc62da7be433b009b63eaa25612cd205f17
KylinWu <midnightkylin@gmail.com>
2018-08-08 14:56:14 +0000

    Format contract

--- contracts/DonateTigerPlayer.sol
+++ contracts/DonateTigerPlayer.sol
@@ -5,34 +5,34 @@ import "./Destructible.sol";
 
 contract DonateTigerPlayer is Ownable, Destructible {
 
-	uint256 public minDonateThreshold;
+    uint public minDonateThreshold;
 
-	struct Donator {
-		uint256 currentValue;
-		uint256 totalValue;
-	}	
-
-    mapping(address => Donator) public donator;
- 
-    constructor() public {
-		minDonateThreshold = 5e16;
+    struct Donator {
+        uint currentValue;
+        uint totalValue;
     }
 
-	function donate() public payable {
-		require(msg.value >= minDonateThreshold, "sender value lower than min of donate threshold");
-		donator[msg.sender].currentValue = msg.value;
-		donator[msg.sender].totalValue += msg.value;
-	}
+    mapping(address => Donator) public donator;
 
-	function modifyDonateThreshold(uint256 _minDonateThreshold) public onlyOwner {
-		require(_minDonateThreshold > 0, "min of donate threshold needs > 0");
-		minDonateThreshold = _minDonateThreshold;
-	}
-	
-	function withdrawAll() public onlyOwner {
-		require(address(this).balance > 0, "contract balance empty");
-		uint256 payment = address(this).balance;
-		address(owner).transfer(payment);
-	}
+    constructor(uint threshold) public {
+        minDonateThreshold = threshold;
+    }
+
+    function donate() public payable {
+        require(msg.value >= minDonateThreshold, "sender value lower than min of donate threshold");
+        donator[msg.sender].currentValue = msg.value;
+        donator[msg.sender].totalValue += msg.value;
+    }
+
+    function modifyDonateThreshold(uint _minDonateThreshold) public onlyOwner {
+        require(_minDonateThreshold > 0, "min of donate threshold needs > 0");
+        minDonateThreshold = _minDonateThreshold;
+    }
+
+    function withdrawAll() public onlyOwner {
+        require(address(this).balance > 0, "contract balance empty");
+        uint payment = address(this).balance;
+        address(owner).transfer(payment);
+    }
 
 }
--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -1,64 +1,36 @@
 pragma solidity ^0.4.24;
 
-
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
 contract Ownable {
-  address public owner;
 
+    address public owner;
 
-  event OwnershipRenounced(address indexed previousOwner);
-  event OwnershipTransferred(
-    address indexed previousOwner,
-    address indexed newOwner
-  );
+    event OwnershipRenounced(address indexed previousOwner);
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
 
+    constructor() public {
+        owner = msg.sender;
+    }
 
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  constructor() public {
-    owner = msg.sender;
-  }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
 
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
+    function renounceOwnership() public onlyOwner {
+        emit OwnershipRenounced(owner);
+        owner = address(0);
+    }
 
-  /**
-   * @dev Allows the current owner to relinquish control of the contract.
-   * @notice Renouncing to ownership will leave the contract without an owner.
-   * It will not be possible to call the functions with the `onlyOwner`
-   * modifier anymore.
-   */
-  function renounceOwnership() public onlyOwner {
-    emit OwnershipRenounced(owner);
-    owner = address(0);
-  }
+    function transferOwnership(address _newOwner) public onlyOwner {
+        _transferOwnership(_newOwner);
+    }
 
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param _newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address _newOwner) public onlyOwner {
-    _transferOwnership(_newOwner);
-  }
-
-  /**
-   * @dev Transfers control of the contract to a newOwner.
-   * @param _newOwner The address to transfer ownership to.
-   */
-  function _transferOwnership(address _newOwner) internal {
-    require(_newOwner != address(0));
-    emit OwnershipTransferred(owner, _newOwner);
-    owner = _newOwner;
-  }
+    function _transferOwnership(address _newOwner) internal {
+        require(_newOwner != address(0), "New owner equal old owner");
+        emit OwnershipTransferred(owner, _newOwner);
+        owner = _newOwner;
+    }
 }
